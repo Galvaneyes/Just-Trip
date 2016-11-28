4d166d1ccafb71972ba7515e8098be240f6c04bd
Galvaneyes <m.v.manchev@gmail.com>
2016-11-28 13:38:34 +0200

    Removed mail regex

--- TOUR/server/models/user-model.js
+++ TOUR/server/models/user-model.js
@@ -2,9 +2,12 @@
 "use strict";
 
 const mongoose = require("mongoose");
+//const regex = "/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i";
 
 const userSchema = new mongoose.Schema({
     username: {type: String, validate: /[a-zA-Z0-9]+/, required: true, unique: true},
+    password: {type: String, required: true},
+    email:{type: String, required: true},
     firstname: {type: String, validate: /[a-zA-Z]+/, required: true},
     lastname:{type: String, validate: /[a-zA-Z]+/, required: true},
     age: {type: Number, min:0 , max:150, required: true},
