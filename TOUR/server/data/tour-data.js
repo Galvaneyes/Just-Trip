4bb1d11147eb7f7100db889f07ec2ae91c721539
Galvaneyes <m.v.manchev@gmail.com>
2016-11-28 16:58:46 +0200

    Fill missing implementation

--- TOUR/server/data/tour-data.js
+++ TOUR/server/data/tour-data.js
@@ -1,29 +1,55 @@
 module.exports = function(models) {
-    const tour = models.tour;
-    
-    return {
-        createTour() {
+    const Tour = models.tour;
 
-        },
-        getAaa(){
+    return {
+        createTour(tourInfo) {
+
             return new Promise((resolve, reject) => {
-                console.log("SEARCHING FOR USER COLLECTION...");
-                User.find()
+
+                console.log("CREATING TOUR...");
+
+                Tour.create(tourInfo, (err, tour) => {
+                    if (err) {
+
+                        console.log("CAN NOT CREATE TOUR");
+                        return reject(err);
+                    }
+
+                    console.log("TOUR CREATED!");
+                    return resolve(tour);
+                });
+            });
+        },
+        getTourByRange(page, size) {
+            return new Promise((resolve, reject) => {
+                console.log("SEARCHING FOR TOUR COLLECTION...");
+                Tour.find()
                     .skip(page * size)
                     .limit(size)
-                    .exec((err, users) => {
+                    .exec((err, tours) => {
                         if (err) {
                             console.log("COLLECTION FROM USERS WAS NOT FOUND");
                             return reject(err);
                         }
 
                         console.log("COLLECTION FROM USERS WAS FOUND");
-                        return resolve(users);
+                        return resolve(tours);
                     });
-        });
+            });
         },
         getAllTours() {
+            return new Promise((resolve, reject) => {
+                console.log("SEARCHING FOR ALL TOURS...");
+                Tour.find({}, (err, users) => {
+                    if (err) {
+                        console.log("ERROR WHEN GET ALL TOURS!");
+                        return reject(err);
+                    }
 
+                    console.log("TOURS FOUND!");
+                    return resolve(users);
+                });
+            });
         }
     };
-};
+}
