8ff2fc5a47328f9c87b5cf30513172dea42b129a
Galvaneyes <m.v.manchev@gmail.com>
2016-11-28 16:33:26 +0200

    Fill tour-data functions

--- TOUR/server/data/tour-data.js
+++ TOUR/server/data/tour-data.js
@@ -5,6 +5,23 @@ module.exports = function(models) {
         createTour() {
 
         },
+        getAaa(){
+            return new Promise((resolve, reject) => {
+                console.log("SEARCHING FOR USER COLLECTION...");
+                User.find()
+                    .skip(page * size)
+                    .limit(size)
+                    .exec((err, users) => {
+                        if (err) {
+                            console.log("COLLECTION FROM USERS WAS NOT FOUND");
+                            return reject(err);
+                        }
+
+                        console.log("COLLECTION FROM USERS WAS FOUND");
+                        return resolve(users);
+                    });
+        });
+        },
         getAllTours() {
 
         }
