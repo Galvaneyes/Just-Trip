f0fca2219dfb261dafec82b39d0e723ed794a48d
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-11-29 16:26:35 +0200

    partial middleware for authorisation added

---
+++ TOUR/server/middlewares/auth-middleware.js
@@ -0,0 +1,12 @@
+module.exports = {
+    isAuthenticated(req, res, next) {
+        if(req.isAuthenticated()) {
+            next();
+        } else {
+            res.status(401).json({
+                    success: false,
+                    message: 'Not authorized!'
+                });
+        }
+    }
+}
