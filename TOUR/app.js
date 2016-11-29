2d43ae500a4e7b4dc7aa16d23f08008f2856fc47
Galvaneyes <m.v.manchev@gmail.com>
2016-11-29 11:01:55 +0200

    Add cpnnection to mLab

--- TOUR/app.js
+++ TOUR/app.js
@@ -7,45 +7,45 @@ const app = require("./server/config/application")({ data });
 require("./server/routers")(app, data);
 
 // TEST FOR CREATING AND FINDING
-// const admin = {
-//     username: "admin",
-//     password: "pass",
-//     email: "no@email.com",
-//     firstname: "no",
-//     lastname: "name",
-//     age: 14,
-//     country: "Bulgaria",
-//     city: "Sofia"
-// };
+const admin = {
+    username: "admin",
+    password: "pass",
+    email: "no@email.com",
+    firstname: "no",
+    lastname: "name",
+    age: 14,
+    country: "Bulgaria",
+    city: "Sofia"
+};
 
-// const tour = {
-//     creator: "ghost",
-//     title: "Ebatti eskurziqta",
-//     city: "BambBu",
-//     country:"Lolo",
-//     description: "Puskaite seeeee",
-//     maxUser: 20,
-//     endTourDate: Date.now(),
-//     isValid: true
-// };
+const tour = {
+    creator: "ghost",
+    title: "Ebatti eskurziqta",
+    city: "BambBu",
+    country:"Lolo",
+    description: "Puskaite seeeee",
+    maxUser: 20,
+    endTourDate: Date.now(),
+    isValid: true
+};
 
-// data.getTourById("583c73ca85e5523248668145")
-//     .then(tourId => {
-//         console.log(tourId);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         data.createTour(tour);
-//     })
+data.getTourById("583d42b30d185c440861d6b1")
+    .then(tourId => {
+        console.log(tourId);
+    })
+    .catch(err => {
+        console.log(err);
+        data.createTour(tour);
+    })
 
-// data.getUserByUsername(admin.username)
-    // .then(user => {
-    //     console.log(user);
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    //     data.createUser(admin);
-    // })
+data.getUserByUsername(admin.username)
+    .then(user => {
+        console.log(user);
+    })
+    .catch(err => {
+        console.log(err);
+        data.createUser(admin);
+    })
 
 // END OF TEST
 
