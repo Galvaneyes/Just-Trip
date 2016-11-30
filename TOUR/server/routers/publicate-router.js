b36cb97b6091e9f660cd42084c761346de629121
Galvaneyes <m.v.manchev@gmail.com>
2016-11-30 20:45:11 +0200

    Auth publicate route

--- TOUR/server/routers/publicate-router.js
+++ TOUR/server/routers/publicate-router.js
@@ -2,13 +2,14 @@
 "use strict";
 
 const express = require("express");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 module.exports = function(app, data) {
     const router = new express.Router();
     const publicate = require("../controllers/publicate-controller.js")(data);
 
     router
-        .get("/", publicate.get)
+        .get("/", authMiddleware.isAuthenticated, publicate.get)
         .post("/", publicate.createTour)
 
     app.use("/publicate", router);
