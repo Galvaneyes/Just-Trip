6c61d314b613605996cc2cd18665d996607f4a6a
pavelhristov <pavel.hristov_@abv.bg>
2016-12-05 01:57:41 +0200

    registration switched to ajax request

---
+++ public/scripts/main.js
@@ -0,0 +1,39 @@
+/* globals document $ toastr */
+
+$(document).ready(function() {
+    $("#register-form").on("submit", function(ev) {
+
+        ev.preventDefault();
+
+        console.log("Here");
+        $.ajax({
+            url: "/register",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                username: $("#username-input").val(),
+                firstname: $("#firstname-input").val(),
+                lastname: $("#lastname-input").val(),
+                password: $("#password-input").val(),
+                email: $("#email-input").val(),
+                country: $("#country-input").val(),
+                city: $("#city-input").val(),
+                age: $("#age-input").val(),
+                gender: $("#select").val(),
+                phone: $("#phone").val()
+            }),
+            cache: false,
+            timeout: 10000,
+            success: function() {
+                document.location.href = "/profile";
+            },
+            statusCode: {
+                400: function(response) {
+                    toastr.warning(response.responseText, "Invalid input");
+                }
+            }
+        });
+
+        return false;
+    });
+});
