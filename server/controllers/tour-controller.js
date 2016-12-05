938fbb1f456f24774df2618ad76e66696c14ef59
Galvaneyes <m.v.manchev@gmail.com>
2016-12-05 02:08:26 +0200

    Addde missing properties

--- server/controllers/tour-controller.js
+++ server/controllers/tour-controller.js
@@ -21,9 +21,9 @@ module.exports = function({ data }) {
                         trip: {
                             id: trip._id,
                             creator: trip.creator,
-                            tripTitle: trip.headline,
-                            tripCountry: trip.country,
-                            tripCity: trip.city,
+                            headline: trip.headline,
+                            country: trip.country,
+                            city: trip.city,
                             currentUsers: trip.getUserCount,
                             capacity: trip.maxUser,
                             price: trip.price,
